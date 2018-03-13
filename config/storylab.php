eaa9cdc1588bd39b13f646b5f187d21c9b43d376
Olav Schettler <olav@schettler.net>
2018-03-13 20:09:47 +0100

    Updated knowfox/crud

--- config/storylab.php
+++ config/storylab.php
@@ -81,7 +81,7 @@ return [
         'model' => \App\Scene::class,
         'entity_title' => [' Scene', 'Scenes'],
         'entity_name' => 'scene',
-        'order_by' => 'title',
+        'order_by' => ['story_id', 'weight'],
         'columns' => [
             'title' => 'Title',
             'story.title' => 'Story',
