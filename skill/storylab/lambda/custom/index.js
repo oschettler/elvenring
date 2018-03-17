b4cac131dc44012d10f1160fba44c9a259b6fa5f
Olav Schettler <olav@schettler.net>
2018-03-17 12:22:21 +0100

    API call

--- skill/storylab/lambda/custom/index.js
+++ skill/storylab/lambda/custom/index.js
@@ -12,6 +12,9 @@ const HELP_MESSAGE = 'Starte, indem du "Geschichten" sagst. Wähle eine der Gesc
 const HELP_REPROMPT = 'Wie kann ich dir weiterhelfen?';
 const STOP_MESSAGE = 'Tschüss!';
 
+const API_HOST = 'storylab.schettler.net';
+const API_PORT = 443;
+
 const data = [
     'Plom vom Planeten Gurgius',
     'Abenteuer im Abteil'
@@ -41,12 +44,18 @@ const handlers = {
             text = '';
         }
 
-        text += 'Ich habe diese ' + data.length + ' Geschichten für dich: "'
-            + data.join(', ') + '"';
+        api('/api/stories/2', data => {
+            const stories = data.data;
 
-        this.response.cardRenderer(SKILL_NAME, text);
-        this.response.speak(text);
-        this.emit(':responseReady');
+            text += 'Ich habe diese ' + stories.length + ' Geschichten für dich: "'
+            + stories.join(', ') + '"';
+
+            console.log(text);
+
+            this.response.cardRenderer(SKILL_NAME, text);
+            this.response.speak(text);
+            this.emit(':responseReady');
+        });
     },
     'AMAZON.HelpIntent': function () {
         const speechOutput = HELP_MESSAGE;
@@ -64,3 +73,29 @@ const handlers = {
         this.emit(':responseReady');
     },
 };
+
+const https = require('https');
+
+function api(path, callback) {
+    let req = https.request({
+        host: API_HOST,
+        port: API_PORT,
+        path,
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjBlMmQ3ZGNlYWQwOGY4ZGQ3N2FiMTMyMWZhZjhjMTc2MGNiNDFjZTUxMGM4N2Q3MGNjM2U2YTk3MDc3ZTIxNDg0MzgzOGVjMTI5YjNlNjQ1In0.eyJhdWQiOiIxIiwianRpIjoiMGUyZDdkY2VhZDA4ZjhkZDc3YWIxMzIxZmFmOGMxNzYwY2I0MWNlNTEwYzg3ZDcwY2MzZTZhOTcwNzdlMjE0ODQzODM4ZWMxMjliM2U2NDUiLCJpYXQiOjE1MjA5NzgzNzksIm5iZiI6MTUyMDk3ODM3OSwiZXhwIjoxNTUyNTE0Mzc5LCJzdWIiOiIyIiwic2NvcGVzIjpbXX0.fc4hQ_iUXBfMEIf-OvFLaPkJPuEMkli1Jse5Fbt9uNMPSSeNPKgm-DPKHfQpWO4wryzAujRhn1swdgdt0lBSjAU-et8-BHjF0XisjZqLiehEqz3Nuda7N33JidKF7vj36p4mJEByNbGJUh2A7_v5nvnSaL1IBcxlKP0J94evHAy-21QQMezl8dDP4ONqSsejXO6Vx-e4REK3ij7FoXIkccXMj4T8N2xOoN_jbuB5m6cm5n9vEIL7CpbPbg7AajmSDfuMBI80Z2GKWoEjqqRvzQYMvoUBeX-7lHH0Xegon3szAUfVVrR4K6j1UJEmXMZ482AdAHyfTtVO7-giStGVP71hadrxcdgpFtKvf7g1K7OZbf-38Qo32A8yLtN00WMXH1YWs-NShcFuKDpNARWyz952CVuIT-GVJVO8gPtWGtCwMd0B7nO9L6PD_d-dz8XfSulRjOsGv0f8beFHAA6yGHd9MDm4O4-tBLwMRu5WE4LWS_tMhaXbsmIbeYW46VHjhjlvag7oJT3wruGzEjCZ-r8aDsSfvQHZLUYu0_dxJ-jneBiuLU01o4sJDrxm35M8nIhZfIYsXSAYtu-T6c_Hxq4Izv6y0E280FE_-krLFbC3ovCCSUYdkOnBl9PwzELec6hFQ0Czi4frREkNX6qSYEeEJ0gV8_yvkcNJsN38n5I'
+        }
+    }, res => {
+        res.setEncoding('utf8');
+        let return_data = '';
+
+        res.on('data', chunk => {
+            return_data += chunk;
+        });
+
+        res.on('end', () => {
+            console.log(JSON.stringify(return_data));
+            callback(JSON.parse(return_data));
+        })
+    });
+}
