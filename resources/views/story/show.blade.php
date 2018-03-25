0df034df4a7f22dc607a9ce6f53993a876d099f7
Olav Schettler <olav@schettler.net>
2018-03-25 15:21:15 +0200

    Wider

--- resources/views/story/show.blade.php
+++ resources/views/story/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-6 mx-auto">
+            <div class="col-10 mx-auto">
 
                 @include('crud::partials.messages')
 
