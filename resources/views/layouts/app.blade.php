6519e6ef7a6536f751e5d303e40f60531990c1e3
Olav Schettler <olav@schettler.net>
2018-03-25 14:32:22 +0200

    Google Validation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <meta name="google-site-verification" content="M4cHX9Fcek68LAve_gP8gEn8lL37KVtkwsRYmbtiR9U">
+
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@lang(config('app.name', 'Laravel'))</title>
